be82cfc8a9f3a018d7d431ce465b34e19797bce0
Alan Lengkoan <alanlengkoan15@gmail.com>
2023-11-21 13:22:58 +0800

    update again

--- fundamental/src/variabels.ts
+++ fundamental/src/variabels.ts
@@ -2,5 +2,12 @@ let nama = "alan lengkoan";
 let umur = 20;
 let isMarried = false;
 
+let myName: string = "alan lengkoan";
+let myAge: number = 20;
+let myStatus: boolean = false;
+
+let myName2: string | number | boolean;
+myName2 = true;
+
 // variabel any
 let level;
